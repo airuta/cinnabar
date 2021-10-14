68aaa428291f4565c83ec642da795266b51e7fd8
Valerii Praid <mori.airuta@gmail.com>
2021-10-14 16:47:42 +0200

    Add edge provider and imrove vertex provider

--- src/graphs/grid.rs
+++ src/graphs/grid.rs
@@ -1,29 +1,38 @@
+use crate::Edges;
 use crate::Index;
-use crate::Nodes;
+use crate::Vertices;
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::default::Default;
 
-pub struct Grid<Idx, Payload> {
+pub struct Grid<I, E, V> {
     rows: i32,
     columns: i32,
-    grid: Vec<Vec<Idx>>,
-    data: HashMap<Idx, (i32, i32, Payload)>,
+    grid: Vec<Vec<I>>,
+    vertices: HashMap<I, (i32, i32, V)>,
+    edges: HashMap<(I, I), E>,
 }
 
-impl<Idx: Index, Payload: Default> Grid<Idx, Payload> {
+impl<I, E, V> Grid<I, E, V>
+where
+    I: Index,
+    E: Default,
+    V: Default,
+{
     pub fn new(rows: i32, columns: i32) -> Self {
-        Self::construct(rows, columns)
+        Self::construct(rows, columns).wireup()
     }
 
     fn construct(rows: i32, columns: i32) -> Self {
-        let mut data = HashMap::new();
+        let mut vertices = HashMap::new();
         let mut grid = Vec::with_capacity(rows as usize);
         for r in 0..rows {
             let mut row = Vec::with_capacity(columns as usize);
             for c in 0..columns {
                 let idx = Index::generate();
+                let vertex = (r, c, V::default());
+                vertices.insert(idx, vertex);
                 row.push(idx);
-                data.insert(idx, (r, c, Payload::default()));
             }
             grid.push(row)
         }
@@ -31,23 +40,42 @@ impl<Idx: Index, Payload: Default> Grid<Idx, Payload> {
             rows,
             columns,
             grid,
-            data,
+            vertices,
+            edges: HashMap::new(),
         }
+    }
+
+    fn wireup(mut self) -> Self {
+        for row in 0..self.rows as usize {
+            for (a, b) in (0..self.columns as usize).tuple_windows() {
+                let a = self.grid[row][a];
+                let b = self.grid[row][b];
+                self.edges.insert((a, b), E::default());
+            }
+        }
+        for column in 0..self.rows as usize {
+            for (a, b) in (0..self.rows as usize).tuple_windows() {
+                let a = self.grid[a][column];
+                let b = self.grid[b][column];
+                self.edges.insert((a, b), E::default());
+            }
+        }
+        self
     }
 }
 
 // Coords interface
 
-impl<Idx: Index, Payload> Grid<Idx, Payload> {
-    pub fn get(&self, row: i32, column: i32) -> Option<Idx> {
+impl<I: Index, E, V> Grid<I, E, V> {
+    pub fn get(&self, row: i32, column: i32) -> Option<I> {
         self.grid
             .get(row as usize)
             .and_then(|row| row.get(column as usize))
             .copied()
     }
 
-    pub fn coords_of(&self, id: Idx) -> (i32, i32) {
-        let (row, column, _) = self.data.get(&id).unwrap();
+    pub fn coords_of(&self, id: I) -> (i32, i32) {
+        let (row, column, _) = self.vertices.get(&id).unwrap();
         (*row, *column)
     }
 
@@ -60,23 +88,52 @@ impl<Idx: Index, Payload> Grid<Idx, Payload> {
     }
 }
 
-// Nodes provider
+// Vertices provider
 
-impl<Idx: Index, Payload> Nodes<Idx> for Grid<Idx, Payload> {
-    type NodeIter<'a> = impl Iterator<Item = Idx>;
-    type NeighborIter<'a> = impl Iterator<Item = Idx>;
-
-    fn nodes(&self) -> Self::NodeIter<'_> {
-        self.grid.iter().flat_map(|row| row.iter()).copied()
-    }
+impl<I: Index, E, V> Vertices<I> for Grid<I, E, V> {
+    type VertexIter<'a> = impl Iterator<Item = I>;
+    type NeighborIter<'a> = impl Iterator<Item = I>;
 
     fn order(&self) -> usize {
-        self.rows as usize * self.columns as usize
+        self.vertices.len()
     }
 
-    fn neighbors(&self, id: Idx) -> Self::NeighborIter<'_> {
+    fn nodes(&self) -> Self::VertexIter<'_> {
+        self.vertices.keys().copied()
+    }
+
+    fn neighbors(&self, id: I) -> Self::NeighborIter<'_> {
         let (row, column) = self.coords_of(id);
         self.neighbors_of(row, column)
             .map(|(row, column)| self.get(row, column).unwrap())
+    }
+
+    fn has_vertex(&self, id: I) -> bool {
+        self.vertices.contains_key(&id)
+    }
+}
+
+// Edges provider
+
+impl<I: Index, E, V> Edges<I> for Grid<I, E, V> {
+    type EdgeIter<'a> = impl Iterator<Item = (I, I)>;
+    type OutIter<'a> = impl Iterator<Item = (I, I)>;
+
+    fn size(&self) -> usize {
+        self.edges.len()
+    }
+
+    fn edges(&self) -> Self::EdgeIter<'_> {
+        self.edges.keys().copied()
+    }
+
+    fn out(&self, id: I) -> Self::OutIter<'_> {
+        let (row, column) = self.coords_of(id);
+        self.neighbors_of(row, column)
+            .map(move |(row, column)| (id, self.get(row, column).unwrap()))
+    }
+
+    fn has_edge(&self, source: I, target: I) -> bool {
+        self.edges.contains_key(&(source, target))
     }
 }
--- src/providers.rs
+++ src/providers.rs
@@ -1,8 +1,19 @@
-pub trait Nodes<Idx> {
-    type NodeIter<'a>: Iterator<Item = Idx>;
-    type NeighborIter<'a>: Iterator<Item = Idx>;
+pub trait Vertices<I> {
+    type VertexIter<'a>: Iterator<Item = I>;
+    type NeighborIter<'a>: Iterator<Item = I>;
 
-    fn nodes(&self) -> Self::NodeIter<'_>;
     fn order(&self) -> usize;
-    fn neighbors(&self, id: Idx) -> Self::NeighborIter<'_>;
+    fn nodes(&self) -> Self::VertexIter<'_>;
+    fn neighbors(&self, id: I) -> Self::NeighborIter<'_>;
+    fn has_vertex(&self, id: I) -> bool;
+}
+
+pub trait Edges<I> {
+    type EdgeIter<'a>: Iterator<Item = (I, I)>;
+    type OutIter<'a>: Iterator<Item = (I, I)>;
+
+    fn size(&self) -> usize;
+    fn edges(&self) -> Self::EdgeIter<'_>;
+    fn out(&self, id: I) -> Self::OutIter<'_>;
+    fn has_edge(&self, source: I, target: I) -> bool;
 }
