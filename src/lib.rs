7dcad53d6d3b4f78552da79b2a5fe1c2779ebc79
Valerii Praid <mori.airuta@gmail.com>
2021-10-14 13:03:58 +0200

    Remove default test from lib

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,3 @@ mod index;
 
 pub use edge::Edge;
 pub use index::Index;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
