4a43eb084b07aceb0397441bf50eb986a4d57afd
Mori Airuta <mori.airuta@gmail.com>
2021-10-28 00:52:30 +0200

    Write documentation for providers

--- src/providers.rs
+++ src/providers.rs
@@ -1,13 +1,31 @@
+//! This modules defines tratis the provide topologies for a graph. In general, a graph
+//! implementation provides two topologie, one for edges and one for vertices. They are
+//! typically represented by separate structs implementing the [`Topology`] trait.
+
 use crate::topology::Topology;
 
+/// This trait defines methods to access overall number of vertices (graph order) and
+/// a vertex topology.
 pub trait VertexProvider<I> {
+    /// A particular implementation of a topology trait for vertices.
     type Vertices<'a>: Topology<Item = I>;
+
+    /// `order` retruns the number of vertices in a graph.
     fn order(&self) -> usize;
+
+    /// `vertices` returns the instance of vertex topology.
     fn vertices(&self) -> Self::Vertices<'_>;
 }
 
+/// This trait defines methods to access overall number of edges (graph size) and
+/// a vertex topology.
 pub trait EdgeProvider<I> {
+    /// A particular implementation of a topology trait for edges.
     type Edges<'a>: Topology<Item = (I, I)>;
+
+    /// `size` retruns the number of edges in a graph.
     fn size(&self) -> usize;
+
+    /// `vertices` returns the instance of edge topology.
     fn edges(&self) -> Self::Edges<'_>;
 }
