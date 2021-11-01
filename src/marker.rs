f1a096e3ca100c9c4bb5ce1b3780e38a197ab908
Mori Airuta <mori.airuta@gmail.com>
2021-11-01 12:10:35 +0100

    Fix unidirectional marker name

--- src/marker.rs
+++ src/marker.rs
@@ -1,7 +1,7 @@
 //! This module contains marker structs that modify graph structure like directionality.
 
 /// Marker for unidirectional graphs.
-pub struct Unidirection;
+pub struct Unidirectional;
 
 /// Marker for bidirectional graphs.
 pub struct Bidirectional;
